aa42adacf7980c1e59542186ed4662d8a32ab5d1
olaneat <olaneat@yahoo.com>
2018-07-23 14:32:52 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <skills/>
+  </div>
+</template>
+
+<script>
+import skills from './components/skills.vue'
+
+export default {
+  name: 'app',
+  components: {
+    skills
+  }
+}
+</script>
+
+<style >
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  align-items: center;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
+#app {
+    width: 50%;
+}
+</style>
